8151013047bbc9d7e59bd1198d1137f9cc3049b3
jdiego_reyes <jdiego.reyes@gmail.com>
2024-04-28 17:13:35 +0200

    Move and jump. Deathzone

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
+@onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player.global_position = start_position.global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +14,9 @@ func _process(delta):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+		
+
+
+func _on_death_zone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,43 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 200
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite_2d.flip_h = (direction == -1)
+		
+	velocity.x = direction * speed
+	
+	move_and_slide()
+	
+	update_animations(direction)
+	
+
+func update_animations(direction: int):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite_2d.play("jump")
+		else:
+			animated_sprite_2d.play("fall")	
+
+func die():
+	get_tree().reload_current_scene()
