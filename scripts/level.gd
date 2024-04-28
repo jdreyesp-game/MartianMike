0b24bb4c43bfaf37be8077f3a773738128ea0109
jdiego_reyes <jdiego.reyes@gmail.com>
2024-04-28 18:32:57 +0200

    Animations for traps, groups and singnals

--- scripts/level.gd
+++ scripts/level.gd
@@ -6,8 +6,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.global_position = start_position.global_position
-
-
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		trap.touched_player.connect(_on_trap_touched_player)
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -18,5 +20,12 @@ func _process(delta):
 
 
 func _on_death_zone_body_entered(body):
-	body.velocity = Vector2.ZERO
-	body.global_position = start_position.global_position
+	reset_player()
+
+
+func _on_trap_touched_player():
+	reset_player()
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start_position.global_position
